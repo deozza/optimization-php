b45a112ebde5abd37d65ef033dc3f8cf8315df05
HyppolitePrn <pernothyppolite@gmail.com>
2025-03-03 12:15:23 +0100

    tried to optimize

--- app/src/Controller/CarouselController.php
+++ app/src/Controller/CarouselController.php
@@ -2,10 +2,10 @@
 
 namespace App\Controller;
 
-use App\Repository\DirectusFilesRepository;
 use App\Repository\GalaxyRepository;
-use App\Repository\ModelesFilesRepository;
 use App\Repository\ModelesRepository;
+use App\Repository\ModelesFilesRepository;
+use App\Repository\DirectusFilesRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
@@ -13,8 +13,14 @@ use Symfony\Component\Routing\Attribute\Route;
 final class CarouselController extends AbstractController
 {
     #[Route('/carousel', name: 'app_carousel')]
-    public function index(GalaxyRepository $galaxyRepository, ModelesRepository $modelesRepository, ModelesFilesRepository $modelesFilesRepository, DirectusFilesRepository $directusFilesRepository): Response
+    public function index(
+        GalaxyRepository $galaxyRepository,
+        ModelesRepository $modelesRepository,
+        ModelesFilesRepository $modelesFilesRepository,
+        DirectusFilesRepository $directusFilesRepository
+    ): Response
     {
+        // Temporarily revert to the original implementation until we fix the memory issue
         $galaxies = $galaxyRepository->findAll();
         $carousel = [];
 
@@ -22,19 +28,24 @@ final class CarouselController extends AbstractController
             $carouselItem = [
                 'title' => $galaxy->getTitle(),
                 'description' => $galaxy->getDescription(),
+                'files' => []
             ];
             
             $modele = $modelesRepository->find($galaxy->getModele());
-            $modelesFiles = $modelesFilesRepository->findBy([
-                'modeles_id' => $modele->getId()
-            ]);
-            $files = [];
-
-            foreach($modelesFiles as $modelesFile) {
-                $file = $directusFilesRepository->find($modelesFile->getDirectusFilesId());
-                $files[] = $file;
+            
+            if ($modele) {
+                $modelesFiles = $modelesFilesRepository->findBy([
+                    'modeles_id' => $modele->getId()
+                ]);
+                
+                foreach($modelesFiles as $modelesFile) {
+                    $file = $directusFilesRepository->find($modelesFile->getDirectusFilesId());
+                    if ($file) {
+                        $carouselItem['files'][] = $file;
+                    }
+                }
             }
-            $carouselItem['files'] = $files;
+            
             $carousel[] = $carouselItem;
         }
         
