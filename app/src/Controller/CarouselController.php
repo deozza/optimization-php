82eb9482d75a97c0e1f331c42f845bc9267f1034
Oceloott <baptiste.blst@gmail.com>
2025-03-03 11:04:44 +0100

    feat(2): compressing img/optimisation twig

--- app/src/Controller/CarouselController.php
+++ app/src/Controller/CarouselController.php
@@ -2,10 +2,8 @@
 
 namespace App\Controller;
 
-use App\Repository\DirectusFilesRepository;
 use App\Repository\GalaxyRepository;
-use App\Repository\ModelesFilesRepository;
-use App\Repository\ModelesRepository;
+
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
@@ -13,7 +11,7 @@ use Symfony\Component\Routing\Attribute\Route;
 final class CarouselController extends AbstractController
 {
     #[Route('/carousel', name: 'app_carousel')]
-    public function index(GalaxyRepository $galaxyRepository, ModelesRepository $modelesRepository, ModelesFilesRepository $modelesFilesRepository, DirectusFilesRepository $directusFilesRepository): Response
+    public function index(GalaxyRepository $galaxyRepository): Response
     {
         $carousel = $galaxyRepository->fetchGalaxiesWithFiles();
 
